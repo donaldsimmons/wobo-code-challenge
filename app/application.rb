9422e16db503db0a00bfc648ef9e23ca08c2407c
Trey Simmons <yert511@gmail.com>
2013-10-21 14:47:37 -0500

    Add render method to display templates

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,15 @@ require "haml"
 
 class Application
 	def call(env)
-		[200, {"Content-Type" => "text/plain"}, ["Hello World"]] 
+		@request = Rack::Request.new(env)
+    
+		case @request.path
+			when "/" then Rack::Response.new(render("index.html"))
+		end
+	end
+
+	def render(template)
+		path = File.expand_path("../views/#{template}", __FILE__)
+		File.read(path)
 	end
 end
