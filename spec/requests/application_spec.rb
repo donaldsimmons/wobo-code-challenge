ec103ffa8aa008098ff853c3b12ff3922e52e1b0
Trey Simmons <yert511@gmail.com>
2013-10-21 14:46:19 -0500

    Create test for routing to index page

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -1,1 +1,11 @@
 require "spec_helper"
+require File.expand_path("../../../app/application", __FILE__)
+
+describe Application, :type => :feature do
+	let(:request) { Rack::MockRequest.new(Application) }
+	subject { request }
+	
+	it "/ returns Index Page" do
+		expect { request.get("/").body }.to have_selector "form" 
+	end
+end
