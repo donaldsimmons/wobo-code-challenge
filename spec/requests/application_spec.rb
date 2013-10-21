2a0fc46759f890b517973205af8eac68d241c260
Trey Simmons <yert511@gmail.com>
2013-10-21 15:23:29 -0500

    Write test to check for routing of '/books'

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -1,11 +1,16 @@
 require "spec_helper"
+require "haml"
 require File.expand_path("../../../app/application", __FILE__)
 
 describe Application, :type => :feature do
 	let(:request) { Rack::MockRequest.new(Application) }
 	subject { request }
 	
-	it "/ returns Index Page" do
-		expect { request.get("/").body }.to have_selector "form" 
+	it "/ returns index page" do
+		expect { request.get("/").body }.to have_content "Search for Books"
+	end
+
+	it "returns results page with /books" do
+		expect { request.get("/books").body }.to have_content "Book Results"
 	end
 end
