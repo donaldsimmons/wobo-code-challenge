0ae79749b80b3faf3d8385e556c3d95ea5842d61
Trey Simmons <yert511@gmail.com>
2013-10-22 14:49:22 -0500

    Write tests for BookSearch class

--- spec/requests/application_spec.rb
+++ spec/requests/application_spec.rb
@@ -3,14 +3,15 @@ require "haml"
 require File.expand_path("../../../app/application", __FILE__)
 
 describe Application, :type => :feature do
-	let(:request) { Rack::MockRequest.new(Application) }
-	subject { request }
-	
-	it "/ returns index page" do
-		expect { request.get("/").body }.to have_content "Search for Books"
+	before do
+	 	@request = Rack::MockRequest.new(Application)
+	end
+
+	it "renders search form when given root url" do
+		@request.get("/").body.must_include "Search for Books"
 	end
 
 	it "returns results page with /books" do
-		expect { request.get("/books").body }.to have_content "Book Results"
+		expect { request.get("/books").body }.to have_text "Search Results"
 	end
 end
