30ce095a6d4cebad642b87de9f5a1319910b6d55
Trey Simmons <yert511@gmail.com>
2013-10-22 14:48:31 -0500

    Initialize Vacuum object for use in response

--- app/application.rb
+++ app/application.rb
@@ -7,12 +7,18 @@ class Application
 
 	def initialize(env)
 		@request = Rack::Request.new(env)
+		@api_object = Vacuum.new.configure({
+	    aws_access_key_id:     "AKIAISKMDJ5IH3UVK4OQ",
+			aws_secret_access_key: "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",
+			associate_tag: 				 "dpsimmons-20"	
+		})
 	end  
 
 	def response
 		case @request.path
-			when "/" then Rack::Response.new(render("index.html.haml"))
-			when "/books" then Rack::Response.new(render("books.html.haml"))
+			when "/"      then Rack::Response.new(render("index.html.haml"))
+			when "/books" 
+				Rack::Response.new(render("books.html.haml"))
 			else Rack::Response.new("None Found", 404)
 		end
 	end
